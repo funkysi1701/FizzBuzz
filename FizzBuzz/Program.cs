3ca426c64c06c36a014d066d8aa2ee7b074cf952
Simon Foster <funkysi1701@gmail.com>
2017-10-08 18:37:05 +0100

    Initial Commit of the FIzzBuzz problem

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FizBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if(i % 5 == 0 && i % 3 == 0) { Console.WriteLine("FizzBuzz"); }
+                else if (i % 5 == 0) { Console.WriteLine("Buzz"); }
+                else if (i % 3 == 0) { Console.WriteLine("Fizz"); }
+                else Console.WriteLine(i);
+                
+            }
+            Console.Read();
+        }
+    }
+}
